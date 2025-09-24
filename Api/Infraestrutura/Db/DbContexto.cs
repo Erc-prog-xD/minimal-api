bdaae1c61f3aa2c7543458297833c8adda5c47ff
Eric Araujo <ericalbuquerque8@hotmail.com>
2025-09-24 16:08:41 -0300

    Ajustes

--- Api/Infraestrutura/Db/DbContexto.cs
+++ Api/Infraestrutura/Db/DbContexto.cs
@@ -1,43 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dominio.Entidades;
 
-namespace MinimalApi.Infraestrutura.Db;
-
-public class DbContexto : DbContext
+namespace MinimalApi.Infraestrutura.Db
 {
-    private readonly IConfiguration _configuracaoAppSettings;
-    public DbContexto(IConfiguration configuracaoAppSettings)
+    public class DbContexto : DbContext
     {
-        _configuracaoAppSettings = configuracaoAppSettings;
-    }
-
-    public DbSet<Administrador> Administradores { get; set; } = default!;
-    public DbSet<Veiculo> Veiculos { get; set; } = default!;
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Administrador>().HasData(
-            new Administrador {
-                Id = 1,
-                Email = "administrador@teste.com",
-                Senha = "123456",
-                Perfil = "Adm"
-             }
-        );
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if(!optionsBuilder.IsConfigured)
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options)
         {
-            var stringConexao = _configuracaoAppSettings.GetConnectionString("MySql")?.ToString();
-            if(!string.IsNullOrEmpty(stringConexao))
-            {
-                optionsBuilder.UseMySql(
-                    stringConexao,
-                    ServerVersion.AutoDetect(stringConexao)
-                );
-            }
+        }
+
+        public DbSet<Administrador> Administradores { get; set; } = default!;
+        public DbSet<Veiculo> Veiculos { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>().HasData(
+                new Administrador {
+                    Id = 1,
+                    Email = "administrador@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
         }
     }
 }
